29eb32d8e9414db89f0ff58da26aba380007437a
asj92kr <asj92kr@naver.com>
2023-08-29 15:02:15 +0900

    server

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000);
+const handleListen = () => console.log('Listening on http://localhost');
+
+app.listen(3000, handleListen);
