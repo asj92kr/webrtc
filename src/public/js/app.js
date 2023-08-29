59b62ae91d64db88abbba62687d6dc08a8e87a23
asj92kr <asj92kr@naver.com>
2023-08-29 17:45:45 +0900

    server -> browser soket connetion

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-hello;
+const soket = new WebSocket(`ws://${window.location.host}`);
+
+soket.addEventListener("open", () => {
+    console.log("서버에 연결됨");
+});
+
+soket.addEventListener("close", () => {
+    console.log("서버와 연결 종료");
+});
+
+soket.addEventListener("message", (message) => {
+    console.log("이 메세지는 : " , message.data , "로부터 받아옴");
+});
+
+setTimeout(() => {
+    soket.send("브라우저 -> 서버 메세지");
+}, 5000);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSoket from "ws";
 import express from "express";
 
 const app = express();
@@ -6,7 +8,29 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log('Listening on http://localhost');
 
-app.listen(3000, handleListen);
+//http서버
+const server = http.createServer(app);
+//WebSoket서버
+const wss = new WebSoket.Server({server});
+
+
+wss.on("connection", (soket) =>{
+    //console.log(soket);
+    console.log("브라우저 연결됨");
+
+    soket.on("close", () => {
+        console.log("브라우저 연결 끊김");
+    });
+    soket.on("message", (message) => {
+        console.log(message.toString('utf8'));
+    })
+    
+    soket.send("서버");
+    
+} );
+
+server.listen(3000, handleListen);
